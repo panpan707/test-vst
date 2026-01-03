dcf82e67a478d2acda943187cc749cf5ed0cf59b
Phan <nguyenhaphan07112005@gmail.com>
2026-01-04 00:02:45 +0700

    add action (ex: add names, add tree type,...)

--- WinApp/SQL/Procs.sql
+++ WinApp/SQL/Procs.sql
@@ -1,91 +1,219 @@
-﻿use KTPM 
-go
+﻿USE KTPM
+GO
 
-drop proc updateDonVi
-go
-create proc updateDonVi
-( @action int
-, @Id int output
-, @Ten nvarchar(50) = NULL
-, @HanhChinhId int = NULL
-, @TenHanhChinh nvarchar(50) = NULL
-, @TrucThuocId int = NULL
-) as
+-- =============================================
+-- PHẦN 1: HỆ THỐNG (ĐÃ SỬA LẠI LOGIC ACTION)
+-- Action: 1=Insert, 2=Update, 3=Delete
+-- =============================================
+
+CREATE OR ALTER PROC updateDonVi
+( 
+    @action int,
+    @Id int output,
+    @Ten nvarchar(50) = NULL,
+    @HanhChinhId int = NULL,
+    @TenHanhChinh nvarchar(50) = NULL,
+    @TrucThuocId int = NULL
+) AS
 BEGIN
-    if @action = -1
-    begin
-        delete from DonVi where Id = @Id
-        return
-    end
-    if @action = 0
-    begin
-        update DonVi set
+    -- 3: XÓA
+    IF @action = 3 
+    BEGIN
+        DELETE FROM DonVi WHERE Id = @Id
+        RETURN
+    END
+
+    -- 2: CẬP NHẬT
+    IF @action = 2 
+    BEGIN
+        UPDATE DonVi SET
             Ten = @Ten,
             HanhChinhId = @HanhChinhId,
             TenHanhChinh = @TenHanhChinh,
             TrucThuocId = @TrucThuocId
-            where Id = @Id
-        return
-    end
-    insert into DonVi values (
-        @Ten,@HanhChinhId,@TenHanhChinh,@TrucThuocId
-    )
-    set @Id = @@IDENTITY
-END
-go
+        WHERE Id = @Id
+        RETURN
+    END
 
-drop proc updateHoSo
-go
-create proc updateHoSo
-( @action int
-, @Id int output
-, @TenDangNhap varchar(50)
-, @Ten nvarchar(50) = NULL
-, @SDT varchar(50) = NULL
-, @Email varchar(50) = NULL
-, @Ext text = NULL
-, @MatKhau varchar(255) = NULL
-, @QuyenId int = NULL
-) as
+    -- 1: THÊM MỚI
+    INSERT INTO DonVi (Ten, HanhChinhId, TenHanhChinh, TrucThuocId)
+    VALUES (@Ten, @HanhChinhId, @TenHanhChinh, @TrucThuocId)
+    
+    SET @Id = @@IDENTITY
+END
+GO
+
+CREATE OR ALTER PROC updateHoSo
+( 
+    @action int,
+    @Id int output,
+    @TenDangNhap varchar(50) = NULL,
+    @Ten nvarchar(50) = NULL,
+    @SDT varchar(50) = NULL,
+    @Email varchar(50) = NULL,
+    @Ext text = NULL,
+    @MatKhau varchar(255) = NULL,
+    @QuyenId int = NULL
+) AS
 BEGIN
-    if @action = -1
-    begin
-		delete from TaiKhoan where Ten = @TenDangNhap
-        delete from HoSo where Id = @Id
-        return
-    end
-    if @action = 0
-    begin
-        update HoSo set
+    IF @action = 3
+    BEGIN
+        -- Xóa tài khoản trước, hồ sơ sau
+        DELETE FROM TaiKhoan WHERE HoSoId = @Id
+        DELETE FROM HoSo WHERE Id = @Id
+        RETURN
+    END
+
+    IF @action = 2
+    BEGIN
+        UPDATE HoSo SET
             Ten = @Ten,
             SDT = @SDT,
             Email = @Email,
             Ext = @Ext
-            where Id = @Id
-        return
-    end
+        WHERE Id = @Id
+        
+        -- Nếu có đổi quyền thì cập nhật bảng TaiKhoan
+        IF @QuyenId IS NOT NULL AND @TenDangNhap IS NOT NULL
+            UPDATE TaiKhoan SET QuyenId = @QuyenId WHERE Ten = @TenDangNhap
 
-	declare @fk int
-	set @fk = (select HoSoId from TaiKhoan where Ten = @TenDangNhap)
-	if @fk is null
-	begin
-		insert into HoSo values (@Ten,@SDT,@Email,@Ext)
-		set @Id = @@IDENTITY
-		insert into TaiKhoan values (@TenDangNhap,@MatKhau,@QuyenId,@Id)
-	end
+        RETURN
+    END
+
+    -- Thêm mới
+    INSERT INTO HoSo (Ten, SDT, Email, Ext) VALUES (@Ten, @SDT, @Email, @Ext)
+    SET @Id = @@IDENTITY
+    
+    INSERT INTO TaiKhoan (Ten, MatKhau, QuyenId, HoSoId) 
+    VALUES (@TenDangNhap, @MatKhau, @QuyenId, @Id)
 END
-go
+GO
 
-drop proc changePass
-go
+-- =============================================
+-- PHẦN 2: NGHIỆP VỤ RỪNG (BỔ SUNG MỚI)
+-- =============================================
 
-create proc changePass
-( @action int
-, @Ten varchar(50)
-, @MatKhau varchar(255)
-) as
+CREATE OR ALTER PROCEDURE updateLoRung
+    @action INT, 
+    @Id INT = NULL OUTPUT,
+    @MaLo NVARCHAR(50) = NULL,
+    @DienTich FLOAT = 0,
+    @TruLuong FLOAT = 0,
+    @NamTrong INT = NULL,
+    @NguonGoc NVARCHAR(50) = NULL,
+    @DieuKienLapDia NVARCHAR(255) = NULL,
+    @TrangThaiSuDung NVARCHAR(100) = NULL,
+    @DonViId INT = NULL,
+    @LoaiRungId INT = NULL,
+    @ChuRungId INT = NULL,
+    @GiongCayId INT = NULL,
+    @KyQuyHoachId INT = NULL,
+    @BanDo NVARCHAR(MAX) = NULL 
+AS
 BEGIN
-	update TaiKhoan set MatKhau = @MatKhau
-	where Ten = @Ten
+    IF @action = 3 
+    BEGIN
+        DELETE FROM LoRung WHERE Id = @Id;
+        RETURN;
+    END
+
+    IF @action = 2
+    BEGIN
+        UPDATE LoRung SET 
+            MaLo = @MaLo, DienTich = @DienTich, TruLuong = @TruLuong, 
+            NamTrong = @NamTrong, NguonGoc = @NguonGoc, DieuKienLapDia = @DieuKienLapDia, 
+            TrangThaiSuDung = @TrangThaiSuDung, DonViId = @DonViId, LoaiRungId = @LoaiRungId, 
+            ChuRungId = @ChuRungId, GiongCayId = @GiongCayId, KyQuyHoachId = @KyQuyHoachId, BanDo = @BanDo
+        WHERE Id = @Id;
+        RETURN;
+    END
+
+    INSERT INTO LoRung (MaLo, DienTich, TruLuong, NamTrong, NguonGoc, DieuKienLapDia, TrangThaiSuDung, DonViId, LoaiRungId, ChuRungId, GiongCayId, KyQuyHoachId, BanDo)
+    VALUES (@MaLo, @DienTich, @TruLuong, @NamTrong, @NguonGoc, @DieuKienLapDia, @TrangThaiSuDung, @DonViId, @LoaiRungId, @ChuRungId, @GiongCayId, @KyQuyHoachId, @BanDo);
+    SET @Id = @@IDENTITY;
 END
-go
+GO
+
+-- Các bảng danh mục đơn giản (LoaiRung, ChuRung, GiongCay...)
+-- Framework có thể tự sinh SQL nhưng tốt nhất nên viết Proc nếu giáo viên yêu cầu
+CREATE OR ALTER PROC updateLoaiRung (@action int, @Id int output, @TenLoai nvarchar(255), @MaLoai varchar(50)=NULL, @MoTa nvarchar(max)=NULL) AS
+BEGIN
+    IF @action=3 BEGIN DELETE FROM LoaiRung WHERE Id=@Id RETURN END
+    IF @action=2 BEGIN UPDATE LoaiRung SET TenLoai=@TenLoai, MaLoai=@MaLoai, MoTa=@MoTa WHERE Id=@Id RETURN END
+    INSERT INTO LoaiRung (TenLoai, MaLoai, MoTa) VALUES (@TenLoai, @MaLoai, @MoTa) SET @Id=@@IDENTITY
+END
+GO
+
+CREATE OR ALTER PROC updateChuRung (@action int, @Id int output, @TenChuRung nvarchar(255), @LoaiChuSoHuu nvarchar(100)=NULL, @DiaChi nvarchar(max)=NULL, @SoDienThoai nvarchar(20)=NULL) AS
+BEGIN
+    IF @action=3 BEGIN DELETE FROM ChuRung WHERE Id=@Id RETURN END
+    IF @action=2 BEGIN UPDATE ChuRung SET TenChuRung=@TenChuRung, LoaiChuSoHuu=@LoaiChuSoHuu, DiaChi=@DiaChi, SoDienThoai=@SoDienThoai WHERE Id=@Id RETURN END
+    INSERT INTO ChuRung (TenChuRung, LoaiChuSoHuu, DiaChi, SoDienThoai) VALUES (@TenChuRung, @LoaiChuSoHuu, @DiaChi, @SoDienThoai) SET @Id=@@IDENTITY
+END
+GO
+
+-- =============================================
+-- PHẦN 3: NGHIỆP VỤ THIÊN TAI (BỔ SUNG MỚI)
+-- =============================================
+
+CREATE OR ALTER PROCEDURE updateDiemThienTai
+    @action INT,
+    @Id INT = NULL OUTPUT,
+    @TenDiem NVARCHAR(255) = NULL,
+    @LoaiThienTai NVARCHAR(50) = NULL,
+    @MucDo NVARCHAR(50) = NULL,
+    @ToaDoX FLOAT = 0,
+    @ToaDoY FLOAT = 0,
+    @MoTa NVARCHAR(MAX) = NULL,
+    @DonViId INT = NULL
+AS
+BEGIN
+    IF @action = 3 
+    BEGIN 
+        DELETE FROM DiemThienTai WHERE Id = @Id; RETURN; 
+    END
+
+    IF @action = 2
+    BEGIN
+        UPDATE DiemThienTai SET 
+            TenDiem = @TenDiem, LoaiThienTai = @LoaiThienTai, MucDo = @MucDo, 
+            ToaDoX = @ToaDoX, ToaDoY = @ToaDoY, MoTa = @MoTa, DonViId = @DonViId
+        WHERE Id = @Id;
+        RETURN;
+    END
+
+    INSERT INTO DiemThienTai (TenDiem, LoaiThienTai, MucDo, ToaDoX, ToaDoY, MoTa, DonViId)
+    VALUES (@TenDiem, @LoaiThienTai, @MucDo, @ToaDoX, @ToaDoY, @MoTa, @DonViId);
+    SET @Id = @@IDENTITY;
+END
+GO
+
+CREATE OR ALTER PROCEDURE updateBaoCao
+    @action INT,
+    @Id INT = NULL OUTPUT,
+    @TieuDe NVARCHAR(255) = NULL,
+    @NgayBaoCao DATETIME = NULL,
+    @NoiDung NVARCHAR(MAX) = NULL,
+    @FileDinhKem NVARCHAR(MAX) = NULL,
+    @NguoiBaoCao VARCHAR(50) = NULL
+AS
+BEGIN
+    IF @action = 3 
+    BEGIN 
+        DELETE FROM BaoCao WHERE Id = @Id; RETURN; 
+    END
+
+    IF @action = 2
+    BEGIN
+        UPDATE BaoCao SET 
+            TieuDe = @TieuDe, NgayBaoCao = @NgayBaoCao, 
+            NoiDung = @NoiDung, FileDinhKem = @FileDinhKem, NguoiBaoCao = @NguoiBaoCao
+        WHERE Id = @Id;
+        RETURN;
+    END
+
+    INSERT INTO BaoCao (TieuDe, NgayBaoCao, NoiDung, FileDinhKem, NguoiBaoCao)
+    VALUES (@TieuDe, @NgayBaoCao, @NoiDung, @FileDinhKem, @NguoiBaoCao);
+    SET @Id = @@IDENTITY;
+END
+GO
